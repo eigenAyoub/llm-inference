b7bb167c6452393a4a6868d49e48dd9342790fe2
eigenAyoub <benayad7@outlook.com>
2025-10-25 23:53:46 +0200

    nit

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import asyncio
 from redis.asyncio import Redis
 import json, uuid
 from pydantic import BaseModel
-from collections import defaultdict
 
 from fastapi import FastAPI, BackgroundTasks, Request, Response, HTTPException
 from fastapi.responses import StreamingResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 
-import time
-from datetime import datetime, timedelta
+from datetime import timedelta, datetime
 
 from numpy.random import randint
 import secrets
@@ -36,22 +34,20 @@ llm_things = {
 q_size = 32
 date_format = "%Y-%m-%d %H:%M:%S.%f"
 
-active_jobs: dict[str, asyncio.Queue] = {}
-toks_per_job: dict[str, asyncio.Queue] = {}
-
 session_ttl = timedelta(hours=1)
-
-## Redis mirrors:
 
 r = Redis(host="localhost", port=6379, decode_responses=True)
 
 async def xadd_h(token: str, stream_id: str, job_id: str,  idx: int):
+    print(f"token {token} to tokens:{stream_id}")
     if token == "eos":
         token_type = "eos"
     else:
         token_type = "token"
 
-    return await r.xadd(
+    # t_id 
+      
+    tid = await r.xadd(    
         f"tokens:{stream_id}",
         {
             "token":  token,
@@ -59,22 +55,14 @@ async def xadd_h(token: str, stream_id: str, job_id: str,  idx: int):
             "idx": idx,
             "job_id": job_id,
             "stream": stream_id
-        })
+        },
+        maxlen=500, 
+        approximate=True # this is the default, ~
+        )
 
+    await r.expire(f"tokens:{stream_id}", 3600)
+    return tid
 
-# should probably be a hash? or set?
-
-async def xadd_session(sid: str):
-    # add a session to the list of sessions
-    return await r.xadd(
-        f"sessions",
-        {
-            "sid":  sid,
-            "ttl": str(datetime.now() + session_ttl)
-        })
-
-
-# sid_to_redis_id = dict()    # sid > redis_id
 
 ## set stuff: stream to sessions: to test membership:
 async def sadd_stream_to_sid(sid: str, stream_id: str):
@@ -83,83 +71,88 @@ async def sadd_stream_to_sid(sid: str, stream_id: str):
 async def sismember_stream_to_sid(sid: str, stream_id: str):
     return await r.sismember(f"sid:{sid}:streams", f"{stream_id}")
 
-async def sremvove_stream_from_sid(sid: str, stream_id: str):
-    return await r.srem(f"sid:{sid}:streams", f"{stream_id}")
 
-# dealing with sessions:
+# dealing with streams in redis hashes:
+async def hadd_stream(sid, stream_id):
+    return await r.hset(
+        f"stream:{stream_id}",
+        mapping={
+            "sid": sid,
+            "n_jobs": 0,
+            "born": str(datetime.now())
+        }
+    )
 
 async def create_session():
     sid = secrets.token_urlsafe(32)
-    #sid_to_redis_id[sid] = await xadd_session(sid)
     await r.set(f"sid:{sid}", "ok", ex=3600)
-    print(f"From create_session:  We have just created a session {sid}")
-    print(f"Does the redis string sid:{sid} exists > {await r.get(f"sid:{sid}")}")
     return sid
+
+
 
 @app.post("/submit_job")
 async def submitjob(prompt: Prompt, request: Request, bg_tasks: BackgroundTasks):
     # print(f"cookies from submit:\n{request.cookies}")
+
     job_id = uuid.uuid4().hex
+
     sid = request.cookies.get("sid")
 
     if not sid:
         raise HTTPException(status_code=401, detail="un-auth")
-    
-    if (await r.ttl(f"{sid}:sid")) > 0:
+
+    if not (await r.exists(f"sid:{sid}")):
         raise HTTPException(status_code=401, detail="you really can't submit a job")
 
-    r.expire(f"sid:{sid}", 3600)
+    await r.expire(f"sid:{sid}", 3600)
 
     stream_id = prompt.stream_id
+    
+    c1 = await r.exists(f"stream:{stream_id}")
 
-    if not (await r.sismember(f"sid:{sid}:streams",f"{stream_id}")) :
-        print(f"stream_id {stream_id} is not part of the streams registered for the session {sid}")
-        raise HTTPException(status_code=403, detail="sid is fine, but can't recognize stream_id")
+    if c1 == 0:
+        raise HTTPException(status_code=403, detail="sid is fine, stream does not exist!")
+
+    c2 = await r.hget(f"stream:{stream_id}", "sid")  
+
+    if c2 != sid:
+        print(f"c2 = {c2}")
+        raise HTTPException(status_code=403, detail="Stream doesn't belong to sid c2")
+
+    is_mem = await r.sismember(f"sid:{sid}:streams", stream_id)
+
+    if not is_mem:
+        print(f"sid = {sid} // {stream_id} is not a mem of  {sid} is_mem = {is_mem}")
+        raise HTTPException(status_code=403, detail=f"Stream {stream_id} not member of sid:{sid}:streams")
+
+
+    await r.expire(f"stream:{stream_id}", 3600) # hash
+    await r.expire(f"sid:{sid}:streams", 3600)  # set
+    await r.hincrby(f"stream:{stream_id}", "n_jobs", 1)
+    
+
+    print(f"sid and stream_id are okay, going to schedule the job")
 
     # this is scheduled after the ack is sent.
     bg_tasks.add_task(generate, prompt.stream_id, job_id)
     return {"received": job_id, "msg": prompt.prompt}
 
-
 async def generate(stream_id: str, job_id: str):
 
-    ready_q = active_jobs.get(stream_id)
-
-    if ready_q is None:
-        return
-
-    if job_id in toks_per_job:
-        # maybe we started it before!
-        return
-
-    toks_per_job[job_id] = asyncio.Queue(maxsize=q_size)
+    # full redis take-over:
+    # i simply just add everything to the same stream of tokens.
 
     reply_id = randint(1, 11)
     tokens = llm_things[str(reply_id)].split()
+
     await asyncio.sleep(0.5)
 
     for idx, tok  in enumerate(tokens):
-        if active_jobs.get(stream_id) is None:
-            toks_per_job.pop(job_id, None)
-            return
+        _ = await xadd_h(tok, stream_id, job_id, idx)
+        await asyncio.sleep(0.1)
 
-        # redis mirror 
-        id_tok = await xadd_h(tok, stream_id, job_id, idx)
-
-        await toks_per_job[job_id].put((tok, id_tok))
-
-        if idx == 0:                      # first token => advertise job once
-            await ready_q.put(job_id)
-         
-        await asyncio.sleep(0.2)
-
-    if active_jobs.get(stream_id) is None:
-        toks_per_job.pop(job_id, None)
-        return
-
-    # redis mirror 
-    id_tok = await xadd_h("eos", stream_id, job_id, len(tokens))
-    await toks_per_job[job_id].put(("eos", id_tok))
+    _ = await xadd_h("eos", stream_id, job_id, len(tokens))
+    print(f"Job {job_id} is done at tokens:{stream_id}")
 
 def sse_event(job_id, token, t_id):
     if token == "eos":
@@ -177,44 +170,59 @@ def sse_event(job_id, token, t_id):
 def sse_heartbeat():
     return ": heartbeat\n\n"
 
-
 async def fan_in(stream_id: str):
-    try:
-        job_id = await asyncio.wait_for(active_jobs[stream_id].get(), timeout=4.0)
-        tok, rid = await toks_per_job[job_id].get()
-        # redis mirror
 
+    cursor = await r.get(f"stream:{stream_id}:cursor")
+    next_token = await r.xread({f"tokens:{stream_id}": cursor}, count = 1, block = 3000)
+
+    if next_token != []:
+        next_token = next_token[0][1][0]
+        rid = next_token[0] 
+        tok = next_token[1]["token"]
+        job_id = next_token[1]["job_id"]
+        await r.set(f"stream:{stream_id}:cursor", rid)
         yield job_id, tok, rid
         if tok == "eos":
-            toks_per_job.pop(job_id)  # remove it
-        else:
-            active_jobs[stream_id].put_nowait(job_id)   # <— unconditional requeue
+            #toks_per_job.pop(job_id)  # remove it
+            await r.hincrby(f"stream:{stream_id}","n_jobs", -1)
+    else:
+        yield -1,-1,-1
 
-    except asyncio.TimeoutError:
-        yield -1, "dead", "-1"
-        pass
+
+
 
 
 # you got some SSE syntax here:
 @app.get("/events")
 async def stream(request: Request):
 
+    # stream creation happens here:
+    
     sid = request.cookies.get("sid")
     
     if not sid:
         raise HTTPException(status_code=401, detail="un-auth")
 
-    # print(f"inside events/: {await r.get(f"sid:{sid}")} {await r.ttl(f"sid:{sid}")}")
-    if (await r.ttl(f"{sid}:sid")) > 0:
+    if not (await r.exists(f"sid:{sid}")):
         raise HTTPException(status_code=401, detail="you really can't submit a job")
-
 
     if (await r.scard(f"sid:{sid}:streams"))>5:
         raise HTTPException(status_code=429, detail="too many streams per sessions")
 
+
     stream_id = uuid.uuid4().hex
-    active_jobs[stream_id] = asyncio.Queue(maxsize=q_size)
-    await sadd_stream_to_sid(sid, stream_id)
+
+    await r.expire(f"sid:{sid}", 3600)
+
+    await r.sadd(f"sid:{sid}:streams", stream_id)
+    print(f" is {stream_id} part of sid:{sid}:streams? {await r.sismember(f"sid:{sid}:streams", stream_id)}")
+    await hadd_stream(sid, stream_id)          # add hash to stream 
+    print(f"we just added {stream_id} hash > {await r.hgetall(f"stream:{stream_id}")}")
+
+    await r.expire(f"sid:{sid}:streams", 3600)
+    await r.expire(f"stream:{stream_id}", 3600)
+    await r.set(f"stream:{stream_id}:cursor","0-0")
+
 
     async def stream_():
         try:
@@ -226,31 +234,19 @@ async def stream(request: Request):
             yield "retry: 2000\n\n"
             yield ": connected\n\n"
             while True:
+
                 async for job, tok, t_id in fan_in(stream_id):
-                    if tok == "dead":
+                    if tok == -1:
                         yield sse_heartbeat()
                     else:
+                        gang = [f"sid:{sid}", f"sid:{sid}:streams", f"stream:{stream_id}", f"tokens:{stream_id}"]
+                        (await r.expire(k, 3600) for k in gang)
                         yield sse_event(job, tok, t_id)
         except asyncio.CancelledError:
             raise
         finally:
+            #await r.srem(f"sid:{sid}:streams", stream_id)
             pass
-            await sremvove_stream_from_sid(sid, stream_id)
-            s = await r.get(f"sid:{sid}")
-            print(f"you have to fix this shit.")
-            if s:
-                s.discard(stream_id)
-
-            q = active_jobs.get(stream_id)
-            if q:
-                try:
-                   while True:
-                        q.get_nowait()
-                except asyncio.QueueEmpty:
-                    pass
-                active_jobs.pop(stream_id, None)
-
-            # TODO: redis clean-up
 
     return StreamingResponse(
         stream_(),
@@ -269,11 +265,15 @@ async def index(response: Response, request: Request):
 
     sid = request.cookies.get("sid")
 
-    if not await r.get(f"{sid}:sid"):
+    if not await r.exists(f"sid:{sid}"):
         sid = await create_session()
         print(f"we've just created {sid} at redis key ttl is {await r.ttl(f"sid:{sid}")} ")
     else:
-        print("session id is in sessions, new stream will be created in events")
+        print(f"session id {sid} is in sessions, new stream will be created in events")
+        print(f"ttl = {await r.ttl(f"sid:{sid}")}")
+        await r.expire(f"sid:{sid}", 3600)
+        print(f"after ttl = {await r.ttl(f"sid:{sid}")}")
+        
 
     response = FileResponse("static/index.html")
 
